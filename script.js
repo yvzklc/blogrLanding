18a8733adf549d64ce1b7f65bcecca2c8f265ee6
yvzklc <kilciyavuz34@gmail.com>
2023-10-15 19:25:14 +0300

    js added

---
+++ script.js
@@ -0,0 +1,27 @@
+function drop1() {
+    var x = document.querySelector(".drop")
+    x.style.display = "block";
+  }
+  
+  function drop2() {
+      var x = document.querySelector(".drop")
+      x.style.display = "none";
+  }
+function drop3() {
+    var x = document.querySelector(".drop1")
+    x.style.display = "block";
+  }
+  
+  function drop4() {
+      var x = document.querySelector(".drop1")
+      x.style.display = "none";
+  }
+function drop5() {
+    var x = document.querySelector(".drop2")
+    x.style.display = "block";
+  }
+  
+  function drop6() {
+      var x = document.querySelector(".drop2")
+      x.style.display = "none";
+  }
