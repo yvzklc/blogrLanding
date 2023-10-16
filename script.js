a45ca29c181402181e5273b4c8b5fa8e0a1dd4e4
yvzklc <kilciyavuz34@gmail.com>
2023-10-16 16:16:15 +0300

    top section has been finished

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 function drop1() {
-    var x = document.querySelector(".drop")
+    var x = document.querySelector("#drop")
+    x.setAttribute('class', 'textclass');
     x.style.display = "flex";
     x.style.flexDirection = "column";
+    x.style.marginLeft = "23.7rem"
+    x.style.flexWrap = "wrap"
+  
+
   }
   
-  function drop6() {
-      var x = document.querySelector(".drop")
+function drop2() {
+      var x = document.querySelector("#drop")
       x.style.display = "none";
   }
 function drop3() {
-    var x = document.querySelector(".drop1")
-    x.style.display = "block";
+    var x = document.querySelector("#drop1")
+    x.setAttribute('class', 'textclass');
+    x.style.display = "flex";
+    x.style.flexDirection = "column";
+    x.style.marginLeft = "30.7rem"
+    x.style.flexWrap = "wrap"
   }
   
-  function drop4() {
-      var x = document.querySelector(".drop1")
+function drop4() {
+      var x = document.querySelector("#drop1")
       x.style.display = "none";
   }
 function drop5() {
-    var x = document.querySelector(".drop2")
-    x.style.display = "block";
+    var x = document.querySelector("#drop2")
+    x.setAttribute('class', 'textclass');
+    x.style.display = "flex";
+    x.style.flexDirection = "column";
+    x.style.marginLeft = "37.7rem"
+    x.style.flexWrap = "wrap"
   }
   
   function drop6() {
-      var x = document.querySelector(".drop2")
+      var x = document.querySelector("#drop2")
       x.style.display = "none";
   }
